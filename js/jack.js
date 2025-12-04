1e96b7812f8993e85c95ca2c69a047c09eb9dd55
Jack <jacktheck02@proton.me>
2025-12-04 01:47:29 -0700

    Landing page

--- js/jack.js
+++ js/jack.js
@@ -1,3 +1,150 @@
+function Legend(color, {
+  title,
+  tickSize = 6,
+  width = 320, 
+  height = 44 + tickSize,
+  marginTop = 18,
+  marginRight = 0,
+  marginBottom = 16 + tickSize,
+  marginLeft = 0,
+  ticks = width / 64,
+  tickFormat,
+  tickValues
+} = {}) {
+
+  function ramp(color, n = 256) {
+    const canvas = document.createElement("canvas");
+    canvas.width = n;
+    canvas.height = 1;
+    const context = canvas.getContext("2d");
+    for (let i = 0; i < n; ++i) {
+      context.fillStyle = color(i / (n - 1));
+      context.fillRect(i, 0, 1, 1);
+    }
+    return canvas;
+  }
+
+  const svg = d3.create("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("viewBox", [0, 0, width, height])
+      .style("overflow", "visible")
+      .style("display", "block");
+
+  let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+  let x;
+
+  // Continuous
+  if (color.interpolate) {
+    const n = Math.min(color.domain().length, color.range().length);
+
+    x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
+
+    svg.append("image")
+        .attr("x", marginLeft)
+        .attr("y", marginTop)
+        .attr("width", width - marginLeft - marginRight)
+        .attr("height", height - marginTop - marginBottom)
+        .attr("preserveAspectRatio", "none")
+        .attr("xlink:href", ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
+  }
+
+  // Sequential
+  else if (color.interpolator) {
+    x = Object.assign(color.copy()
+        .interpolator(d3.interpolateRound(marginLeft, width - marginRight)),
+        {range() { return [marginLeft, width - marginRight]; }});
+
+    svg.append("image")
+        .attr("x", marginLeft)
+        .attr("y", marginTop)
+        .attr("width", width - marginLeft - marginRight)
+        .attr("height", height - marginTop - marginBottom)
+        .attr("preserveAspectRatio", "none")
+        .attr("xlink:href", ramp(color.interpolator()).toDataURL());
+
+    // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+    if (!x.ticks) {
+      if (tickValues === undefined) {
+        const n = Math.round(ticks + 1);
+        tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+      }
+      if (typeof tickFormat !== "function") {
+        tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+      }
+    }
+  }
+
+  // Threshold
+  else if (color.invertExtent) {
+    const thresholds
+        = color.thresholds ? color.thresholds() // scaleQuantize
+        : color.quantiles ? color.quantiles() // scaleQuantile
+        : color.domain(); // scaleThreshold
+
+    const thresholdFormat
+        = tickFormat === undefined ? d => d
+        : typeof tickFormat === "string" ? d3.format(tickFormat)
+        : tickFormat;
+
+    x = d3.scaleLinear()
+        .domain([-1, color.range().length - 1])
+        .rangeRound([marginLeft, width - marginRight]);
+
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.range())
+      .join("rect")
+        .attr("x", (d, i) => x(i - 1))
+        .attr("y", marginTop)
+        .attr("width", (d, i) => x(i) - x(i - 1))
+        .attr("height", height - marginTop - marginBottom)
+        .attr("fill", d => d);
+
+    tickValues = d3.range(thresholds.length);
+    tickFormat = i => thresholdFormat(thresholds[i], i);
+  }
+
+  // Ordinal
+  else {
+    x = d3.scaleBand()
+        .domain(color.domain())
+        .rangeRound([marginLeft, width - marginRight]);
+
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.domain())
+      .join("rect")
+        .attr("x", x)
+        .attr("y", marginTop)
+        .attr("width", Math.max(0, x.bandwidth() - 1))
+        .attr("height", height - marginTop - marginBottom)
+        .attr("fill", color);
+
+    tickAdjust = () => {};
+  }
+
+  svg.append("g")
+      .attr("transform", `translate(0,${height - marginBottom})`)
+      .call(d3.axisBottom(x)
+        .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+        .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+        .tickSize(tickSize)
+        .tickValues(tickValues))
+      .call(tickAdjust)
+      .call(g => g.select(".domain").remove())
+      .call(g => g.append("text")
+        .attr("x", marginLeft)
+        .attr("y", marginTop + marginBottom - height - 6)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .attr("font-weight", "bold")
+        .attr("class", "title")
+        .text(title));
+
+  return svg.node();
+}
+
 const margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 800 - margin.left - margin.right,
     height = 600 - margin.top - margin.bottom;
@@ -45,40 +192,6 @@ d3.csv("data/CostvsEarnings.csv").then( function(data) {
   const color = d3.scaleSequential(d3.interpolateOrRd)
     .domain([0, d3.max(bins, d => d.length)]);
 
-  var Tooltip = d3.select("#jack-section")
-    .append("div")
-    .style("opacity", 0)
-    .attr("class", "tooltip")
-    .style("position", "absolute")
-    .style("background-color", "white")
-    .style("border", "solid")
-    .style("border-width", "2px")
-    .style("border-radius", "5px")
-    .style("padding", "5px")
-
-  var mouseover = function() {
-    Tooltip
-      .style("opacity", 1)
-    d3.select(this)
-      .style("stroke", "black")
-  }
-  var mousemove = function(event, d) {
-    const indices = d.map(p => points.find(pt => pt.x === p[0] && pt.y === p[1]).o);
-    var averageRoi = 0;
-    for (i = 0; i < indices.length; i++) {
-      averageRoi += ((indices[i].MD_EARN_WNE_P10 - (indices[i].COSTT4_A * 4)) / (indices[i].COSTT4_A * 4)) / indices.length;
-    }
-
-    Tooltip
-      .html(`Average ROI: ${averageRoi.toFixed(2)}<br>Percentage of colleges: ${((indices.length / data.length) * 100).toFixed(2)}%`)
-      .style("left", (d.x + 70) + "px")
-      .style("top", (d.y + 50) + "px");
-  }
-  var mouseleave = function() {
-    Tooltip
-      .style("opacity", 0)
-  }
-
   hexBinSvg.append("clipPath")
       .attr("id", "clip")
     .append("rect")
@@ -110,11 +223,8 @@ d3.csv("data/CostvsEarnings.csv").then( function(data) {
       .attr("transform", function(d) { return `translate(${d.x}, ${d.y})`})
       .attr("fill", function(d) { return color(d.length); })
       .attr("stroke", "black")
-      .attr("stroke-width", 0.1)
-    .on("mouseover", mouseover)
-    .on("mousemove", mousemove)
-    .on("mouseleave", mouseleave);
-
+      .attr("stroke-width", 0.1);
+      
   hexBinSvg.append("path")
     .datum(lineData)
     .attr("fill", "none")
@@ -124,6 +234,19 @@ d3.csv("data/CostvsEarnings.csv").then( function(data) {
       .x(function(d) { return x(d.cost) })
       .y(function(d) { return y(d.earnings) })
     )
+  
+  const legendNode = Legend(color, {
+    title: "Institution Bin Count",
+    marginLeft: 5
+  });
+
+  hexBinSvg.append("foreignObject")
+    .attr("x", 20)     
+    .attr("y", 0)
+    .attr("width", 1000)
+    .attr("height", 80)
+    .node()
+    .appendChild(legendNode);
 })
 
 
